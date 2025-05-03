538dbae62c6a432e9d045fd85ece37276f2e42ee
gingrspacecadet <jackrawlings2011@gmail.com>
2025-05-03 14:03:28 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PREFIX="opt/cross"
+TARGET=i686-elf
+PATH="$PREFIX/bin:$PATH"
+
+cd ..
+mkdir src
+mkdir opt
+cd opt
+mkdir cross
+cd ..
+
+cd src
+mkdir build-binutils
+cd build-binutils
+../binutils-2.44/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+cd ..
+
+cd src
+# The $PREFIX/bin dir _must_ be in the PATH. We did that above.
+which -- $TARGET-as || echo $TARGET-as is not in the PATH
+mkdir build-gcc
+cd build-gcc
+../gcc-15.1.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --disable-hosted-libstdcxx
+make all-gcc
+make all-target-libgcc
+make all-target-libstdc++-v3
+make install-gcc
+make install-target-libgcc
+make install-target-libstdc++-v3
