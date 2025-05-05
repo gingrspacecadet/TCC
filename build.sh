7f8c7aa354d38d7b2a0b972bd697796426c7c54b
gingrspacecadet <jackrawlings2011@gmail.com>
2025-05-05 08:34:26 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -27,4 +27,4 @@ make all-target-libstdc++-v3
 make install-gcc
 make install-target-libgcc
 make install-target-libstdc++-v3
-echo "$PREFIX/bin:$PATH" >> $HOME/.bashrc
+echo 'export PATH="$PREFIX/bin:$PATH"' >> $HOME/.bashrc
