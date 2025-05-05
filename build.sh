4eaee405cdfff8aff36e633ce70184bf63cfd89d
gingrspacecadet <jackrawlings2011@gmail.com>
2025-05-05 08:29:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-PREFIX="opt/cross"
+PREFIX="$HOME/opt/cross"
 TARGET=i686-elf
 PATH="$PREFIX/bin:$PATH"
-
-cd ..
-mkdir src
-mkdir opt
-cd opt
-mkdir cross
-cd ..
 
 tar --zstd -xvf binutils-2.44.tar.zst
 tar xf gcc-15.1.0.tar.xz
 rm *.tar.*
 
-cd src
+cd $HOME/src
 mkdir build-binutils
 cd build-binutils
 ../binutils-2.44/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
@@ -22,7 +15,7 @@ make
 make install
 cd ..
 
-cd src
+cd $HOME/src
 # The $PREFIX/bin dir _must_ be in the PATH. We did that above.
 which -- $TARGET-as || echo $TARGET-as is not in the PATH
 mkdir build-gcc
